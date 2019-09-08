d58a63040994869b96ea32d48a258b96517f4e2c
Julian Leahy <leahyjulian@gmail.com>
2019-09-09 11:47:39 +1200

    remove elements and show when array is empty

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import $ from 'jquery';
 import './less/style.less';
+import words from './words';
+
+$(document).ready(function () {
+
+    const init = () => {
+        elementsRemaining();
+    }
+
+    $('.select').click(function (e) {
+        e.preventDefault();
+        const word = words[Math.floor(Math.random() * words.length)];
+        const index = words.indexOf(word);
+        if (index !== -1) {
+            console.log(word);
+            words.splice(index, 1);
+            // no more words
+            if (!elementsRemaining()) {
+                $(this).prop("disabled", true);
+            }
+        }
+    });
+
+
+    const elementsRemaining = () => {
+        const elements = words.length;
+        $('.remain').text(elements)
+        if (elements == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    init();
+
+});
---
+++ src/words.js
@@ -0,0 +1,8 @@
+export const words = [
+    "ant",
+    "tiger",
+    "dog",
+    "bear"
+]
+
+export default words;
